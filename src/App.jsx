6fee909456c053fa01702501b101d0c946cc327a
Vitor Guirardeli Arakaki <vi.arakaki@hotmail.com>
2022-12-13 21:29:10 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import styles from './style'
+
+import { Billing, Business, Button, CardDeal, Clients, CTA, FeedbackCard, Footer, GetStarted, Hero, Navbar, Stats, Testimonials } from './components'
+
+const App = () => (
+  <div className="bg-itau w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar/>
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats/>
+        <Business/>
+        <Footer/>
+      </div>
+    </div>
+
+  </div>
+);
+
+export default App
